c4b290c563dd4395cf9b71617e623171518f0235
Luiz Carlos Ribeiro <luizjunior.dantas@gmail.com>
2020-07-02 13:19:57 -0300

    retirado console do script

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,6 +1,5 @@
 export default function fetchBitcoin(url, target) {
   fetch(url).then((response) => response.json()).then((json) => {
-    console.log(json.BRL.sell);
     const bitcoin = document.querySelector(target);
     bitcoin.innerText = (1000 / json.BRL.sell).toFixed(4);
   }).catch((erro) => {
